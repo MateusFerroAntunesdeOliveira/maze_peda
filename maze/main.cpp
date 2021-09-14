71b345bdb5c4bffaa68543cb42f24c79e7c4dca5
Mateus <mateus.ferro.2001@hotmail.com>
2021-09-14 08:47:57 -0300

    vector labirinto

--- maze/main.cpp
+++ maze/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "Pilha.h"
 
 using namespace std;
 
 int main() {
+
+	vector<string> labirinto = {
+	"XXXXXXXXXXXXXXXXXXXXX",
+	"X     X     X     X X",
+	"XX XX XXXXX X X X   X",
+	"X   X       XXX XXX X",
+	"X X X XXXXXXX   X   X",
+	"X X   X   X X X   X X",
+	"X XXXXX X X   XXXXX X",
+	"X X     X XXX  X    X",
+	"X X X X X   XX XX XXX",
+	"X X XXX X X     X   X",
+	"E X X X X XXXXX XXX X",
+	"X     X X   X X X   X",
+	"X XXXXX XXX X XXX X X",
+	"X X       X X   X X X",
+	"X   XXX X X XXX X X X",
+	"XXX X X X X X X X XXX",
+	"X X   X X   X   X   X",
+	"X XXX XXXXXXXXX XXX X",
+	"X                 X X",
+	"XXXXXXXXXXXXXXXXXXXSX"
+	};
 
     Pilha p;
 
@@ -13,6 +38,12 @@ int main() {
     p.push(1);
     p.push(2);
 
+	cout << "\n* * * MAZE * * *";
+	cout << "\n- Mateus Ferro -\n" << endl;
+
+	for (int i = 0; i < labirinto.size(); i++) {
+		cout << labirinto[i] << endl;
+	}
     cout << "\nTamanho atual da pilha: " << p.tamanho_pilha() << "\n" << endl;
     while (!p.vazia()) {
         cout << "Desempilhando o valor " << p.topo() << endl;
