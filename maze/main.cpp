5678f9b68bc3ca9a8e581574ddb30f8a37a1e3f9
Mateus <mateus.ferro.2001@hotmail.com>
2021-09-28 12:45:52 -0300

    Update vector labirinto

--- maze/main.cpp
+++ maze/main.cpp
@@ -32,28 +32,7 @@ int main() {
 	"XXXXXXXXXXXXXXXXXXXSX"
 	};
 
-	vector<string> labirinto_show = {
-	"XXXXXXXXXXXXXXXXXXXXX",
-	"X     X     X     X X",
-	"XX XX XXXXX X X X   X",
-	"X   X       XXX XXX X",
-	"X X X XXXXXXX   X   X",
-	"X X   X   X X X   X X",
-	"X XXXXX X X   XXXXX X",
-	"X X     X XXX  X    X",
-	"X X X X X   XX XX XXX",
-	"X X XXX X X     X   X",
-	"E X X X X XXXXX XXX X",
-	"X     X X   X X X   X",
-	"X XXXXX XXX X XXX X X",
-	"X X       X X   X X X",
-	"X   XXX X X XXX X X X",
-	"XXX X X X X X X X XXX",
-	"X X   X X   X   X   X",
-	"X XXX XXXXXXXXX XXX X",
-	"X                 X X",
-	"XXXXXXXXXXXXXXXXXXXSX"
-	};
+	vector<string> labirinto_show = labirinto;
 
 	//-> Entrada
 	int x = 10;
@@ -126,7 +105,7 @@ int main() {
 			px.pop();
 			py.pop();
 			--cont_show;
-			//labirinto_show[x][y] = ' ';  -> usar para remover os pontos de 'bifurcacao'
+			//labirinto_show[x][y] = ' ';  //-> usar para remover os pontos de 'bifurcacao'
 		}
 	}
 
